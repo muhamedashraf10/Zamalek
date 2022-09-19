83c0efb4987322956b593dd3cee28bafc024769e
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-09-19 22:55:45 +0200

    Edit navbar

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import { FaUserAlt } from "react-icons/fa";
+import logo from "../../images/zamalek-sc-logo-DBC71FD15E-see@2x.png";
 
 const Navbar = () => {
   return (
@@ -26,14 +27,17 @@ const Navbar = () => {
         <div className="nav2">
           <ul className="lists">
             <li>الرئيسيه</li>
-            <li>الرئيسيه</li>
-            <li>الرئيسيه</li>
-            <li>الرئيسيه</li>
-            <li>الرئيسيه</li>
+            <li>الأخبار</li>
+            <li>تاريخ النادي</li>
+            <li>جدول المباريات</li>
+            <li>فريق العمل</li>
           </ul>
         </div>
       </div>
-      <div className="logo">zamalek</div>
+      <div className="logo">
+        <div className="logo_title"> نادي الزمالك</div>
+        <img src={logo}></img>
+      </div>
     </nav>
   );
 };
